fe8513fa12cf351bef7ef98e91233d96aa357957
joel riquelmy <riquelmejoel04@gmail.com>
2025-10-16 13:57:25 -0300

    criando a contagem regressiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,25 @@
 AOS.init();
+
+const eventDate = new Date("2027-12-12T19:00:00");
+const timestampEvent = eventDate.getTime();
+
+const countTime = setInterval(function() {
+    const now = new Date();
+    const timestampNow = now.getTime();
+
+    const timeLeft = timestampEvent - timestampNow;
+
+    const daysTimeLeft = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    const hoursTimeLeft = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutesTimeLeft = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    const secondsTimeLeft = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+    const countdownElement = document.getElementById("countdown");
+    countdownElement.innerHTML = `${daysTimeLeft}d ${hoursTimeLeft}h ${minutesTimeLeft}min ${secondsTimeLeft}s`;
+
+    if (timeLeft < 0) {
+        clearInterval(countTime);
+        countdownElement.innerHTML = "O evento começou!";
+    }
+
+}, 1000);
